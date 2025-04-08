c4e458c24a8d6b09202947d7b4de9601fe752a27
정명기 <jmk101711@yu.ac.kr>
2025-04-08 21:41:01 +0900

    📦 chore: 데이터 베이스 세팅

---
+++ data/user.ts
@@ -0,0 +1,19 @@
+import { User } from "@/types/db";
+import { user } from "@/db/schema";
+import db from "@/db";
+import { eq } from "drizzle-orm";
+export const getUserByEmail = async (email: string): Promise<User | null> => {
+    try {
+        const existingUser = await db.query.user.findFirst({
+            where: eq(user.email, email),
+        });
+
+        if (!existingUser) {
+            return null;
+        }
+        return existingUser;
+    } catch (error) {
+        console.error(error);
+        throw new Error("문제발생");
+    }
+};
---
+++ types/db.ts
@@ -0,0 +1,2 @@
+import { user } from "@/db/schema";
+export type User = typeof user.$inferSelect;
