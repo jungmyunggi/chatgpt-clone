de2f3bf1beccfbe874098dfe3ddad326b07ad270
정명기 <jmk101711@yu.ac.kr>
2025-04-10 16:43:12 +0900

    ✨ feat: 로그아웃 기능 추가

--- components/chat/LogoutButton.tsx
+++ components/chat/LogoutButton.tsx
@@ -1,4 +1,11 @@
+"use client";
+
+import { deleteSession } from "@/actions/sessions";
 import { Button } from "../ui/button";
 export function LogoutButton() {
-    return <Button className="w-[80%]">로그아웃</Button>;
+    return (
+        <Button className="w-[80%]" onClick={() => deleteSession()}>
+            로그아웃
+        </Button>
+    );
 }
